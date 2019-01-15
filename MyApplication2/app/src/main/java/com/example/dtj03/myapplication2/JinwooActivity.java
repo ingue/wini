e7ff0d800769b43136bfce686d20ce8c09cbdf90
jwjung5038 <jwjung5038@gmail.com>
2019-01-15 11:41:21 +0900

    '진우' 주석 첨가

--- MyApplication2/app/src/main/java/com/example/dtj03/myapplication2/JinwooActivity.java
+++ MyApplication2/app/src/main/java/com/example/dtj03/myapplication2/JinwooActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class JinwooActivity extends AppCompatActivity {
-
+//진우
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
